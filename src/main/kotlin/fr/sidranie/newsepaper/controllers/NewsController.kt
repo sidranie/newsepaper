d0cb18df63822ef575e56bbf93f59a8ffda6a3dc
macdrien <adrien.bouyssou@gmail.com>
2024-10-31 22:32:02 +0100

    ADD CRUD method for News

--- src/main/kotlin/fr/sidranie/newsepaper/controllers/NewsController.kt
+++ src/main/kotlin/fr/sidranie/newsepaper/controllers/NewsController.kt
@@ -1,19 +1,25 @@
 package fr.sidranie.newsepaper.controllers
 
 import fr.sidranie.newsepaper.entities.News
+import fr.sidranie.newsepaper.exceptions.NotFoundException
 import fr.sidranie.newsepaper.services.NewsService
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.net.URI
 
 @RestController
 @RequestMapping("/news")
 class NewsController(private val service: NewsService) {
 
     @GetMapping
-    fun getAllNews(): ResponseEntity<Iterable<News>> =
+    fun getAllNews(): ResponseEntity<List<News>> =
         ResponseEntity.ok(service.findAllNews())
 
     @GetMapping("/{id}")
@@ -24,5 +30,28 @@ class NewsController(private val service: NewsService) {
             ResponseEntity.ok(news)
         else
             ResponseEntity.notFound().build<News>()
+    }
+
+    @PostMapping
+    fun createNews(@RequestBody toCreate: News): ResponseEntity<News> {
+        val news = toCreate.copy(id = null)
+        service.createNews(news)
+        return ResponseEntity.created(URI("/news/${news.id}")).body(news)
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteNews(@PathVariable("id") id: Long): ResponseEntity<News> {
+        service.deleteNews(id)
+        return ResponseEntity.noContent().build()
+    }
+
+    @PatchMapping("{id}")
+    fun updateNews(@PathVariable("id") id: Long, @RequestBody updates: News): ResponseEntity<News> {
+        try {
+            val news = service.patchNews(id, updates)
+            return ResponseEntity.ok(news)
+        } catch (_: NotFoundException) {
+            return ResponseEntity.notFound().build<News>()
+        }
     }
 }
--- src/main/kotlin/fr/sidranie/newsepaper/services/NewsService.kt
+++ src/main/kotlin/fr/sidranie/newsepaper/services/NewsService.kt
@@ -1,11 +1,40 @@
 package fr.sidranie.newsepaper.services
 
 import fr.sidranie.newsepaper.entities.News
+import fr.sidranie.newsepaper.exceptions.NotFoundException
 import fr.sidranie.newsepaper.repositories.NewsRepository
 import org.springframework.stereotype.Service
 
 @Service
 class NewsService(private val repository: NewsRepository) {
-    fun findAllNews(): Iterable<News> = repository.findAll()
-    fun findNewsById(id: Long): News? = repository.findById(id).orElse(null)
+    fun findAllNews(): List<News> = repository.findAll().toList()
+
+    fun findNewsById(id: Long): News? =
+        repository.findById(id).orElse(null)
+
+    fun createNews(news: News) {
+        repository.save<News>(news)
+    }
+
+    fun deleteNews(id: Long) {
+        repository.deleteById(id)
+    }
+
+    fun patchNews(id: Long, updates: News): News {
+        var news = findNewsById(id)
+        if (news == null) {
+            throw NotFoundException()
+        }
+
+        if (updates.headline != null) {
+            news.headline = updates.headline
+        }
+        if (updates.newsBody != null) {
+            news.newsBody = updates.newsBody
+        }
+
+        repository.save(news)
+
+        return news
+    }
 }
